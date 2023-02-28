d89374e0098c865b62e79ea523d026ce2ac24869
Sean Langhi <langhisc@gmail.com>
2023-02-28 15:39:21 -0800

    Add class 'MockNetworkProvider'

---
+++ Sources/Networking/Objects/MockNetworkProvider.swift
@@ -0,0 +1,50 @@
+//
+//  MockNetworkProvider.swift
+//  
+//
+//  Created by Sean Langhi on 2/28/23.
+//
+
+import Combine
+import Foundation
+
+// MARK: - Memory footprint
+
+class MockNetworkProvider: Networking {
+    var simulatedLatency: TimeInterval = 1.5
+    var simulatedError: NetworkError?
+}
+
+// MARK: - Singleton instance
+
+extension MockNetworkProvider {
+    static let shared = MockNetworkProvider()
+}
+
+// MARK: - `Networking` conformance
+
+extension MockNetworkProvider {
+    func sendRequest<R: Requestable>(
+        for requestable: R
+    ) -> AnyPublisher<R.Response, NetworkError> {
+        guard
+            let mockable = requestable as? any Mockable,
+            let response = mockable.mockResponse as? R.Response
+        else {
+            return Fail(error: .noMockAvailable)
+                .delay(for: .init(simulatedLatency), scheduler: RunLoop.main)
+                .eraseToAnyPublisher()
+        }
+        switch simulatedError {
+            case .some(let error):
+                return Fail(error: error)
+                    .delay(for: .init(simulatedLatency), scheduler: RunLoop.main)
+                    .eraseToAnyPublisher()
+            case .none:
+                return Just(response)
+                    .delay(for: .init(simulatedLatency), scheduler: RunLoop.main)
+                    .setFailureType(to: NetworkError.self)
+                    .eraseToAnyPublisher()
+        }
+    }
+}
